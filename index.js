b3b9897eb14fe666b3ff70307acb2115992cd472
Dat-teletubbie <96094945+Dat-teletubbie@users.noreply.github.com>
2023-01-24 18:35:09 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const sidebar = document.getElementById("bigsidebar");
+alert("hifhlskjfhlwrkfgvjh")
+function opensidebar() {
+    sidebar.style.width = "310px"
+    alert("hi")
+}
+function closesidebar() {
+    sidebar.style.width = "0px"
+}
